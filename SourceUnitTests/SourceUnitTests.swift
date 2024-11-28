98249251063b108c4a25f7ef59a15393cfa61a4d
Giovanni Favorin de Melo <Giovannifavorim@gmail.com>
2024-11-28 09:30:36 -0300

    added unit tests and edited targets
    run only on iphone and portrait

---
+++ SourceUnitTests/SourceUnitTests.swift
@@ -0,0 +1,16 @@
+//
+//  SourceUnitTests.swift
+//  SourceUnitTests
+//
+//  Created by Giovanni Favorin de Melo on 28/11/24.
+//
+
+import Testing
+
+struct SourceUnitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
