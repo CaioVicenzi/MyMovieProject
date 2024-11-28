226f28972bc6926675d68828f56b3e69dfb6b33f
CaioVicenzi <80120724+CaioVicenzi@users.noreply.github.com>
2024-11-28 00:30:50 -0300

    feat(OnboardinView):onboarding

---
+++ MyMovieProject/View/Onboarding/InputUsernameView.swift
@@ -0,0 +1,44 @@
+//
+//  InputUsernameView.swift
+//  MyMovieProject
+//
+//  Created by Caio Marques on 28/11/24.
+//
+
+import SwiftUI
+
+struct InputUsernameView: View {
+    @AppStorage("username") var usernameStorage : String = ""
+    @AppStorage("alreadyLog") var alreadyLog : Bool = false
+    
+    @State var username : String = ""
+    @Binding var goHome : Bool
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text("Como devemos te chamar?")
+                TextField("Caio", text: $username)
+                    .padding()
+                    .background(Color.gray.opacity(0.3))
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    .padding(.horizontal)
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button ("Pronto") {
+                        usernameStorage = username
+                        self.goHome = true
+                        self.alreadyLog = true
+                        dismiss()
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    InputUsernameView(goHome: .constant(false))
+}
--- MyMovieProject/View/Onboarding/OnboardingView.swift
+++ MyMovieProject/View/Onboarding/OnboardingView.swift
@@ -2,9 +2,74 @@ import SwiftUI
 
 struct OnboardingView: View {
     @StateObject var vm = OnboardingViewModel()
+    @State var showInputUsername : Bool = false
+    @State var goHome : Bool = false
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            Text("Bem vindo!")
+                .bold()
+                .font(.title)
+            
+            Spacer()
+            Spacer()
+            Spacer()
+            
+            functionalityDescriber("Descubra novos filmes", description: "Procure filmes que te interessam na nossa lista", image: "list.bullet.rectangle", color: .blue)
+            functionalityDescriber("Adicione comentários", description: "Coloque as suas ideias sobre um filme para você ver depois", image: "text.page", color: .yellow)
+            functionalityDescriber("Favorite filmes", description: "Marque os seus filmes favoritos com uma estrelinha!", image: "star.fill", color: .purple)
+            
+            Spacer()
+            Spacer()
+            Spacer()
+            
+            Button {
+                showInputUsername = true
+            } label: {
+                Text("Começar")
+                    .foregroundStyle(.white)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 55)
+                    .background(Color.accentColor)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .padding()
+                    
+
+            }
+            
+            Spacer()
+        }
+        .sheet(isPresented: $showInputUsername) {
+            InputUsernameView(goHome: $goHome)
+                .presentationDetents([.fraction(0.2)])
+        }
+        .navigationDestination(isPresented: $goHome) {
+            HomeView()
+                .navigationBarBackButtonHidden()
+        }
+        
+    }
+    
+    @ViewBuilder
+    func functionalityDescriber (_ title : String, description : String, image : String, color : Color) -> some View {
+        HStack {
+            Image(systemName: image)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 30)
+                .padding(.horizontal)
+                .foregroundStyle(color)
+            
+            VStack (alignment: .leading) {
+                Text(title)
+                    .font(.headline)
+                
+                Text(description)
+            }
+            .frame(maxWidth: 250)
+        }
+        .padding()
     }
 }
 
