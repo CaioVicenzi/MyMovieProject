6f923bb0148a62e63c8b0dab7f7bf65e80a83280
CaioVicenzi <80120724+CaioVicenzi@users.noreply.github.com>
2024-11-27 23:38:49 -0300

    Initial Commit

---
+++ MyMovieProject/MyMovieProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMovieProjectApp.swift
+//  MyMovieProject
+//
+//  Created by Caio Marques on 27/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MyMovieProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
